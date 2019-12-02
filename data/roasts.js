b680e9124100a526567298d71b3b37541f0859ab
taxusstreet <36744747+taxusstreet@users.noreply.github.com>
2019-12-01 22:42:13 -0500

    add Peru and remove Ethiopian and Guatemalan

--- data/roasts.js
+++ data/roasts.js
@@ -1,38 +1,41 @@
-data.roasts = [{
-  "title": "La Fisga",
-  "origin": "Cubulco, Guatemala",
-  "price": 15,
-  "size": 12,
-  "level": 30,
-  "color": "#D34F27",
-  "taste": ["strawberry", "plum", "milk chocolate"],
-  "processing": "Washed",
-  "altitude": "1800m",
-  "variety": "The producer, Roberto Cantã, says he is unsure of the variety. He recieved the seeds from his father-in-law.",
-  "image": "guat.jpg",
-  "button": {
-    "container": "1521071314015",
-    "id": 615224836128
-  },
-  "in-stock": true
-},{
-  "title": "Wote Konga",
-  "origin": "Yergacheffe, Ethiopia",
-  "price": 15,
-  "size": 12,
-  "level": 10,
-  "color": "#924da8",
-  "taste": ["pineapple", "blueberry", "pine"],
-  "processing": "Natural/Dry Processed",
-  "altitude": "1800m-2200m",
-  "variety": "Heirloom",
-  "image": "fix.jpg",
-  "button": {
-    "container": "1521071458284",
-    "id": 615240204320
-  },
-  "in-stock": true
-},{
+data.roasts = [
+//   {
+//   "title": "La Fisga",
+//   "origin": "Cubulco, Guatemala",
+//   "price": 15,
+//   "size": 12,
+//   "level": 30,
+//   "color": "#D34F27",
+//   "taste": ["strawberry", "plum", "milk chocolate"],
+//   "processing": "Washed",
+//   "altitude": "1800m",
+//   "variety": "The producer, Roberto Cantã, says he is unsure of the variety. He recieved the seeds from his father-in-law.",
+//   "image": "guat.jpg",
+//   "button": {
+//     "container": "1521071314015",
+//     "id": 615224836128
+//   },
+//   "in-stock": true
+// },
+//                {
+//   "title": "Wote Konga",
+//   "origin": "Yergacheffe, Ethiopia",
+//   "price": 15,
+//   "size": 12,
+//   "level": 10,
+//   "color": "#924da8",
+//   "taste": ["pineapple", "blueberry", "pine"],
+//   "processing": "Natural/Dry Processed",
+//   "altitude": "1800m-2200m",
+//   "variety": "Heirloom",
+//   "image": "fix.jpg",
+//   "button": {
+//     "container": "1521071458284",
+//     "id": 615240204320
+//   },
+//   "in-stock": true
+// },
+  {
   "title": "El Obraje",
   "origin": "Nariño, Colombia",
   "price": 15,
@@ -49,4 +52,19 @@ data.roasts = [{
     "id": 10620411588
   },
   "in-stock": true
-}];
+},{
+  "title": "Junin",
+  "origin": "Perene, Peru",
+  "price": 15,
+  "size": 12,
+  "level": 65,
+  "color": "#F37A6A",
+  "taste": ["raspberry", "limeade", "buttercream"],
+  "image": "peru.jpg",
+  "button": {
+    "container": "1521069648001",
+    "id": 10620411588
+  },
+  "in-stock": true
+}
+];
