229e9ec94d7563d750050ae188f7c06935d4a8ad
Brent <33323646+brentttt@users.noreply.github.com>
2020-04-01 18:30:32 -0400

    tweak mobile styles

--- bundle.js
+++ bundle.js
@@ -19183,7 +19183,7 @@ var CoffeeShop = function (_Component) {
               null,
               '845 S Battlefield Blvd, Chesapeake, VA 23322'
             ),
-            _react2.default.createElement('iframe', { src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3199.137152491346!2d-76.23423108471304!3d36.69524197997041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89baba89127a40d5%3A0x667afdd32ffd1cd5!2sBig%20Ugly%20Brewing!5e0!3m2!1sen!2sus!4v1585767805172!5m2!1sen!2sus', width: '600', height: '200', frameBorder: '0', style: { border: 0 }, allowFullScreen: '', ariaHidden: 'false', tabIndex: '0' }),
+            _react2.default.createElement('iframe', { src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3199.137152491346!2d-76.23423108471304!3d36.69524197997041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89baba89127a40d5%3A0x667afdd32ffd1cd5!2sBig%20Ugly%20Brewing!5e0!3m2!1sen!2sus!4v1585767805172!5m2!1sen!2sus', width: '100%', height: '200', frameBorder: '0', style: { border: 0 }, allowFullScreen: '', ariaHidden: 'false', tabIndex: '0' }),
             _react2.default.createElement(_Hours2.default, null)
           )
         )
