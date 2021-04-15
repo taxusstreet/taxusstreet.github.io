d78002ad06f99d3d892aa15ce71f8eff8586bc9d
Brent <33323646+brentttt@users.noreply.github.com>
2021-04-14 21:31:24 -0400

    Update cold-brews.js

--- data/cold-brews.js
+++ data/cold-brews.js
@@ -12,19 +12,6 @@ data.coldBrews = [
       "container": "1586477867016",
       "id": 4562922995821
     }
-  },{
-    "title": "Spring Blend",
-    "desc": "A lighter, refreshing brew with floral flavors. Great for your sunny spring stroll while distancing.",
-    "level": "light",
-    "price": 13,
-    "in-stock": true,
-    "image": "coldbrew2.jpg",
-    "color": "salmon",
-    "size": 32,
-    "button": {
-      "container": "1586478026199",
-      "id": 4562922700909
-    }
   } ,{
     "title": "Horchata Blend",
     "desc": "Cold brew with a sweet tooth; we have blended traditional Mexican Horchata with our Original Blend Cold Brew for a blast of rich sweetness from cinnamon sugar, and creaminess from rice milk. Dairy free and delicious, add your favorite creamer to take it to the next level.",
@@ -40,3 +27,18 @@ data.coldBrews = [
     }
   }
 ];
+
+// {
+//   "title": "Spring Blend",
+//   "desc": "A lighter, refreshing brew with floral flavors. Great for your sunny spring stroll while distancing.",
+//   "level": "light",
+//   "price": 13,
+//   "in-stock": true,
+//   "image": "coldbrew2.jpg",
+//   "color": "salmon",
+//   "size": 32,
+//   "button": {
+//     "container": "1586478026199",
+//     "id": 4562922700909
+//   }
+// }
